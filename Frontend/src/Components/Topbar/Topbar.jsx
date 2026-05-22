2179c6e0b59c46c494ad36f70a7233dcab8d6319
Santanu <santanuranjanbal@gmail.com>
2026-05-22 11:34:26 +0530

    pull

---
+++ Frontend/src/Components/Topbar/Topbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Topbar.css";
+import { HiOutlineMail } from "react-icons/hi";
+
+/* IMPORT LOGO IMAGE */
+import logo from "../../assets/logo.png";
+
+const Topbar = () => {
+  return (
+    <div className="topbarMainContainer">
+      <div className="topbarWrapper">
+
+        {/* LEFT SIDE */}
+        <div className="topbarLeftSection">
+          <div className="topbarProjectName">
+            IJPASR
+          </div>
+
+          <div className="topbarDivider"></div>
+
+          <div className="topbarEmailContainer">
+            <div className="topbarEmailItem">
+              <HiOutlineMail className="topbarMailIcon" />
+              <span>editor@ijpasr.com</span>
+            </div>
+
+            <div className="topbarEmailItem">
+              <HiOutlineMail className="topbarMailIcon" />
+              <span>editorijpasr@gmail.com</span>
+            </div>
+          </div>
+        </div>
+
+        {/* CENTER LOGO */}
+        <div className="topbarCenterSection">
+          <img
+            src={logo}
+            alt="IJPASR Logo"
+            className="topbarLogo"
+          />
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="topbarRightSection">
+          <button className="topbarSubmitButton">
+            Submit Paper
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
